dd08e88898fcc3b66b06a96290a34b5e20b256a8
ilia_19011991 <ilia_19011991@mail.ru>
2025-02-17 15:39:03 +0300

    init commit

---
+++ src/main/java/ru/otus/java/basic/homeworks/HelloApplication.java
@@ -0,0 +1,7 @@
+package ru.otus.java.basic.homeworks;
+
+public class HelloApplication {
+    public static void main(String[] args) {
+        System.out.println("Проект домашних заданий Java Basic 2025-01");
+    }
+}
