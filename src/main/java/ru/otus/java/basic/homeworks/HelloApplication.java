6b3ae76dd174d11e94eeeca28eb83170426b79be
ilia_19011991 <ilia_19011991@mail.ru>
2025-02-25 12:45:59 +0300

    homework one done

--- src/main/java/ru/otus/java/basic/homeworks/HelloApplication.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.java.basic.homeworks;
-
-public class HelloApplication {
-    public static void main(String[] args) {
-        System.out.println("Проект домашних заданий Java Basic 2025-01");
-    }
-}
